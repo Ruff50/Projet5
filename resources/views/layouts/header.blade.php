c72916c62c67c41624f12a592db1cf48ee02ac18
Ruff50 <cbolac@gmail.com>
2022-06-13 17:21:33 +0400

    Contrôles travail Arno

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -54,6 +54,7 @@
                    @auth
                    <a href="{{route('signout') }}" class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"> Logout </a>
                    <a href="{{route('Users.index')}}" class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"> Dashboard </a>
+                   <a href="/" class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"> Profil public </a>
                    @endauth
             </div>
         </div>
